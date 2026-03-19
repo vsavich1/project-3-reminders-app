bb841f83858960c3b47a3f7181988fc9151a51ef
Valentyn Savich <vsavich@unc.edu>
2026-03-19 13:13:24 -0400

    Initial Commit

---
+++ project-3-reminders-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project-3-reminders-app
+//
+//  Created by Valentyn Savich on 3/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project-3-reminders-app/project_3_reminders_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_3_reminders_appApp.swift
+//  project-3-reminders-app
+//
+//  Created by Valentyn Savich on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct project_3_reminders_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
