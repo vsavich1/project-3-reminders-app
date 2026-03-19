8011566427d152ba94fe348e41261b6d384f43bc
Valentyn Savich <vsavich@unc.edu>
2026-03-19 15:38:37 -0400

    Finished the List and Footer

--- project-3-reminders-app/ContentView.swift
+++ project-3-reminders-app/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var isEditing: Bool = false
     // TODO: Add an @State property to hold a RemindersPage struct
+    // initialize the page to have something to work with
     @State private var page: RemindersPage = RemindersPage(
         title: "Grocery List",
         items: [Reminder(title: "Apples", isCompleted: false), Reminder(title: "Carrots", isCompleted: false)],
@@ -30,6 +31,8 @@ struct ContentView: View {
                     isEditing = true
                 } label: {
                     Image(systemName: "info.circle")
+                        .font(.title3)
+                        .fontWeight(.semibold)
                 }
             }
             .foregroundStyle(page.color)
@@ -39,6 +42,29 @@ struct ContentView: View {
                 // TODO: Loop through the page's reminders using ForEach
                 ForEach($page.items) { $reminder in
                     // TODO: Display each reminder row
+                    HStack {
+                        // check button
+                        if reminder.isCompleted {
+                            Button {
+                                reminder.isCompleted = false
+                            } label: {
+                                Image(systemName: "circle.fill")
+                                    .foregroundStyle(page.color)
+                            }
+                            .buttonStyle(.plain)
+                        } else {
+                            Button {
+                                reminder.isCompleted = true
+                            } label: {
+                                Image(systemName: "circle")
+                                    .foregroundStyle(page.color)
+                            }
+                            .buttonStyle(.plain)
+                        }
+                        
+                        // editable reminder text
+                        TextField(reminder.title, text: $reminder.title)
+                    }
                 }
                 .onDelete { indexSet in
                     page.items.remove(atOffsets: indexSet)
@@ -47,6 +73,17 @@ struct ContentView: View {
             .listStyle(.plain)
             
             // TODO: Add footer view
+            VStack {
+                Button {
+                    page.items.append(Reminder(title: "", isCompleted: false))
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .foregroundStyle(page.color)
+                        .font(.system(size: 50))
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .padding(.trailing)
 
         }
         .sheet(isPresented: $isEditing) {
