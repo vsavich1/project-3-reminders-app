fd908bc919084a1b201fcfdd46aa4f813613546b
Valentyn Savich <vsavich@unc.edu>
2026-03-19 15:38:12 -0400

    made EditSheet look appealing

--- project-3-reminders-app/EditSheet.swift
+++ project-3-reminders-app/EditSheet.swift
@@ -15,14 +15,31 @@ struct EditSheet: View {
     var body: some View {
         VStack(spacing: 20) {
                 // TODO: Add list.bullet.circle.fill icon and TextField
-            HStack(spacing: 15) {
+            VStack(spacing: 15) {
                 Image(systemName: "list.bullet.circle.fill")
-                    .font(.largeTitle)
+                    .font(.system(size: 70))
+                    .padding(.horizontal)
+                    .padding(.top, 20)
+                    .padding(.bottom, 10)
                 
                 TextField("List Title", text: $title)
+                    .multilineTextAlignment(.center)
                     .font(.largeTitle)
                     .bold()
+                    .padding(.horizontal, 40)
+                    .padding(.vertical, 15)
+                    .background(
+                        RoundedRectangle(cornerRadius: 16)
+                            .foregroundStyle(Color.secondary)
+                            .opacity(0.3)
+                    )
             }
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundStyle(Color.secondary)
+                    .opacity(0.15)
+            )
             
             ColorChooser(selectedColor: $selectedColor)
             
@@ -38,5 +55,5 @@ struct EditSheet: View {
     @Previewable @State var selectedColor: Color = .red
     
     EditSheet(title: $title, selectedColor: $selectedColor)
-        .preferredColorScheme(.dark)
+        //.preferredColorScheme(.dark)
 }
